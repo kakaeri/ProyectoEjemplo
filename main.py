26a52ba8e922372280432af797e6e42b03a8369c
kakaeri <izarocalzu@gmail.com>
2025-03-24 15:56:24 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Mi primera aplicación con Flet"
+    page.add(ft.Text("¡Hola, Flet!"))
+
+ft.app(target=main, view=ft.WEB_BROWSER, port=8080)
