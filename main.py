c358ca1fc223753b87c696271e72f80f79ad9fe6
kakaeri <izarocalzu@gmail.com>
2025-03-26 19:07:22 +0100

    Modificar puerto

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ def main(page: ft.Page):
     page.title = "Mi primera aplicación con Flet"
     page.add(ft.Text("¡Hola, gente de informática!"))
 
-ft.app(target=main, view=ft.WEB_BROWSER, port=8080)
+ft.app(target=main, view=ft.WEB_BROWSER, port=30008)
